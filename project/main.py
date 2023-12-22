0b686b977b1e6116be2704706426ee905139e0ba
Kianoosh <kia.vadaei@gmail.com>
2023-12-22 09:49:20 +0330

    starting

--- project/main.py
+++ project/main.py
@@ -7,9 +7,9 @@ observation = env.reset()
 
 # Define the maximum number of iterations
 NUM_EPISODES = 1000
-
+sum = 0
 for episode in range(NUM_EPISODES):
-
+    env.render()
     # TODO: Implement the agent policy here
     # Note: .sample() is used to sample random action from the environment's action space
 
@@ -22,5 +22,6 @@ for episode in range(NUM_EPISODES):
     if done or truncated:
         observation = env.reset()
 
+print(sum)
 # Close the environment
 env.close()
