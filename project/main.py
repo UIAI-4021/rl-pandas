21c94a768b3bba8fd3636ce14a6b3773f07be952
emika s <mel.shirian@gmail.com>
2023-12-22 10:46:56 +0330

    adding mai parts

--- project/main.py
+++ project/main.py
@@ -1,16 +1,65 @@
 import gym
-import gym_maze
+import numpy as np
 
+
+class Maze:
+
+    @staticmethod
+    def choose_action(state):
+        action = 0
+        epsilon = 0.8
+
+        epsilon_choice = np.random.uniform(0, 1)
+
+        if epsilon_choice < epsilon:
+            action = env.action_space.sample()
+        else:
+            action = np.argmax(Q[state, :])
+        return action
+
+    @staticmethod
+    def update(state, state2, reward, action, action2):
+        predict = Q[state, action]
+        target = reward + 0.8 * Q[state2, action2]
+        Q[state, action] = Q[state, action] + 0.9 * (target - predict)
+
+
+import gym_maze
 # Create an environment
 env = gym.make("maze-random-10x10-plus-v0")
 observation = env.reset()
 
 # Define the maximum number of iterations
 NUM_EPISODES = 1000
-
+Q = np.zeros((100, 4))
 for episode in range(NUM_EPISODES):
+    training = 0
+    state1 = env.reset()
+    action1 = Maze.choose_action(state1)
 
-    # TODO: Implement the agent policy here
+    while training < 100:
+        # Visualizing the training
+        env.render()
+
+        # Getting the next state
+        state2, reward, done, info = env.step(action1)
+
+        # Choosing the next action
+        action2 = Maze.choose_action(state2)
+
+        # Learning the Q-value
+        Maze.update(state1, state2, reward, action1, action2)
+
+        state1 = state2
+        action1 = action2
+
+        # Updating the respective vaLues
+        training += 1
+        reward += 1
+
+        # If at the end of learning process
+        if done:
+            break
     # Note: .sample() is used to sample random action from the environment's action space
 
     # Choose an action (Replace this random action with your agent's policy)
@@ -24,3 +73,4 @@ for episode in range(NUM_EPISODES):
 
 # Close the environment
 env.close()
+
