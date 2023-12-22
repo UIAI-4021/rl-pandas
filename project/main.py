00e2828289b0c3dc3691f2083ec7b8bd1864cc97
emika s <mel.shirian@gmail.com>
2023-12-22 10:59:22 +0330

    adding mai parts

--- project/main.py
+++ project/main.py
@@ -33,8 +33,40 @@ observation = env.reset()
 NUM_EPISODES = 1000
 
 for episode in range(NUM_EPISODES):
+<<<<<<< HEAD
 
     # TODO: Implement the agent policy here
+=======
+    training = 0
+    state1 = 0
+    action1 = Maze.choose_action(state1)
+
+    while training < 100:
+        # Visualizing the training
+        env.render()
+
+        # Getting the next state
+        state2, reward, done, info = env.step(action1)
+        state2 = state2[0] * 10 + state2[1]
+
+        # Choosing the next action
+        action2 = Maze.choose_action(state2)
+
+
+        # Learning the Q-value
+        Maze.update(state1, state2, reward, action1, action2)
+
+        state1 = state2
+        action1 = action2
+
+        # Updating the respective vaLues
+        training += 1
+        reward += 1
+
+        # If at the end of learning process
+        if done:
+            break
+>>>>>>> 5872190 (adding mai parts)
     # Note: .sample() is used to sample random action from the environment's action space
 
     # Choose an action (Replace this random action with your agent's policy)
